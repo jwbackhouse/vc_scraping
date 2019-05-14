19a3c5106f386fb5a053b4cd643f58d4d97895b6
James Backhouse <jwbackhouse@gmail.com>
2019-05-14 09:49:44 +0000

    Output moved to csv to include brand and date.

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var d= new Date();
 
-var d = new Date();
-fs.appendFile("out/vc_restaurants.txt", '\n\n' + d.getMinutes() + ':' + d.getHours() + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'\n', (err) => {
-        if (err) console.log(err);
-        console.log("Date written to file.");
-      });
-
+// Run requests
 request('https://www.vouchercodes.co.uk/bellaitalia.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
-})
-.pipe(fs.createWriteStream('out/vctest.txt'));
+});
 
 request('https://www.vouchercodes.co.uk/pizzaexpress.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -38,11 +35,12 @@ request('https://www.vouchercodes.co.uk/pizzaexpress.com', function (error, resp
 request('https://www.vouchercodes.co.uk/jamieoliver.com-italian', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -51,11 +49,12 @@ request('https://www.vouchercodes.co.uk/jamieoliver.com-italian', function (erro
 request('https://www.vouchercodes.co.uk/lochfyne.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -64,11 +63,12 @@ request('https://www.vouchercodes.co.uk/lochfyne.com', function (error, response
 request('https://www.vouchercodes.co.uk/askrestaurants.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -77,11 +77,12 @@ request('https://www.vouchercodes.co.uk/askrestaurants.com', function (error, re
 request('https://www.vouchercodes.co.uk/byronhamburgers.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -90,11 +91,12 @@ request('https://www.vouchercodes.co.uk/byronhamburgers.com', function (error, r
 request('https://www.vouchercodes.co.uk/zizzi.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -103,11 +105,12 @@ request('https://www.vouchercodes.co.uk/zizzi.co.uk', function (error, response,
 request('https://www.vouchercodes.co.uk/prezzoplc.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -116,11 +119,12 @@ request('https://www.vouchercodes.co.uk/prezzoplc.co.uk', function (error, respo
 request('https://www.vouchercodes.co.uk/wagamama.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -129,11 +133,12 @@ request('https://www.vouchercodes.co.uk/wagamama.com', function (error, response
 request('https://www.vouchercodes.co.uk/frankieandbennys.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -142,11 +147,12 @@ request('https://www.vouchercodes.co.uk/frankieandbennys.com', function (error, 
 request('https://www.vouchercodes.co.uk/caferouge.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -154,11 +160,12 @@ request('https://www.vouchercodes.co.uk/caferouge.co.uk', function (error, respo
 request('https://www.vouchercodes.co.uk/bills-website.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -167,11 +174,12 @@ request('https://www.vouchercodes.co.uk/bills-website.co.uk', function (error, r
 request('https://www.vouchercodes.co.uk/millerandcarter.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -180,11 +188,12 @@ request('https://www.vouchercodes.co.uk/millerandcarter.co.uk', function (error,
 request('https://www.vouchercodes.co.uk/pizzahut', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -193,11 +202,12 @@ request('https://www.vouchercodes.co.uk/pizzahut', function (error, response, ht
 request('https://www.vouchercodes.co.uk/dominos.uk.com', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -207,11 +217,12 @@ request('https://www.vouchercodes.co.uk/dominos.uk.com', function (error, respon
 request('https://www.vouchercodes.co.uk/harvester.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
@@ -220,11 +231,12 @@ request('https://www.vouchercodes.co.uk/harvester.co.uk', function (error, respo
 request('https://www.vouchercodes.co.uk/chiquito.co.uk', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    var brand = $("meta[property='og:title']").attr('content');
     $('.merch-offers article .offer__title').each(function(i, element){
       var a = $(this);
-      fs.appendFile("out/vc_restaurants.txt", a.text().trim() + '\n', (err) => {
+      fs.appendFile("out/vc_rest.csv", '"' + brand + '","' + ('0'+d.getHours()).slice(-2) + ':' + ('0'+d.getMinutes()).slice(-2) + ' ' + d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() +'","' + a.text().trim() + '"\n', (err) => {
         if (err) console.log(err);
-        console.log("Successfully written to file.");
+        console.log("Successfully written " + brand + " to file.");
       });
     });
   }
